fa573c57c800f316aa3b082a49fb64d6316f7918
Harry Manchanda <harmanmanchanda182@gmail.com>
2017-11-20 19:10:33 +0530

    Webpack Basics!

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,34 @@
+/**
+ * Webpack
+ */
+
+import '../css/main.css';
+import '../css/input-elements.css';
+import { secretButton, secretParagraph } from './dom-loader';
+
 var showSecret = false;
 
 secretButton.addEventListener('click', toggleSecretState);
 updateSecretParagraph();
 
 function toggleSecretState() {
-    showSecret = !showSecret;
-    updateSecretParagraph();
-    updateSecretButton()
+  showSecret = !showSecret;
+  updateSecretParagraph();
+  updateSecretButton()
 }
 
 function updateSecretButton() {
-    if (showSecret) {
-        secretButton.textContent = 'Hide the Secret';
-    } else {
-        secretButton.textContent = 'Show the Secret';
-    }
+  if (showSecret) {
+    secretButton.textContent = 'Hide the Secret';
+  } else {
+    secretButton.textContent = 'Show the Secret';
+  }
 }
 
 function updateSecretParagraph() {
-    if (showSecret) {
-        secretParagraph.style.display = 'block';
-    } else {
-        secretParagraph.style.display = 'none';
-    }
+  if (showSecret) {
+    secretParagraph.style.display = 'block';
+  } else {
+    secretParagraph.style.display = 'none';
+  }
 }
